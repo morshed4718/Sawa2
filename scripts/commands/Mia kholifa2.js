2309947bc8fe1212d4d4f39ed68efdd2fcc0fb14
MorshEd <mdmorshed4718@gmail.com>
2024-10-15 02:11:40 +0600

    Create Mia kholifa2.js

---
+++ scripts/commands/Mia kholifa2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "miyakholifa",
+    version: "1.1.1",
+prefix: true,
+	permssion: 0,
+	credits: "John Lester", 
+	description: "Just Respond",
+	category: "no prefix",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	let react = event.body.toLowerCase();
+	if(react.includes("miyakholifa") || react.includes("bossk") || react.includes("karbot")) {
+		var msg = {
+				boday: "বাঈনচোদ Mia kholifa তোর আম্মু লাগে!!💔 লিংক বা ভিডিও লাগলে!! বস 𝗠𝗼𝗿𝘀𝗵𝗘𝗱 কে নক কর মাদারবোর্ড!!FB LINK :https://www.facebook.com/profile.php?id=100075133524968&mibextid=ZbWKwL🤍",
+			}
+			api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
